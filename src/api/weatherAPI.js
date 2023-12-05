4d2965ace0b737a1463bfbd42bbddf341289db74
Motoong <sonada903@gmail.com>
2023-12-05 15:46:59 +0900

    지역 드롭다운메뉴

--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '발급받은 API 키';
+const API_KEY = '7316e0e0316d9b0c9a39787a0a719ea2';
 const API_ENDPOINT = 'https://api.openweathermap.org/data/2.5/weather';
 
 export function getWeatherByCity(city) {
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { getWeatherByCity } from '../api/weatherAPI';
 
 function HomePage() {
   const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState('Seoul');
+
+  const handleCityChange = (e) => {
+    setCity(e.target.value);
+  };
 
   useEffect(() => {
-    getWeatherByCity('Seoul')
+    getWeatherByCity(city)
       .then(response => {
         setWeather(response.data);
       })
       .catch(error => {
         console.error('날씨 정보를 가져오는데 실패했습니다:', error);
       });
-  }, []);
+  }, [city]);
 
   return (
     <div>
       <h1>홈 페이지</h1>
+      <select value={city} onChange={handleCityChange}>
+        <option value="Seoul">서울</option>
+        <option value="Busan">부산</option>
+        <option value="Daegu">대구</option>
+        <option value="Incheon">인천</option>
+        <option value="Gwangju">광주</option>
+        <option value="Daejeon">대전</option>
+        <option value="Ulsan">울산</option>
+        <option value="Sejong">세종</option>
+        <option value="Gyeonggi-do">경기도</option>
+        <option value="Gangwon-do">강원도</option>
+        <option value="Chungcheongbuk-do">충청북도</option>
+        <option value="Chungcheongnam-do">충청남도</option>
+        <option value="Jeollabuk-do">전라북도</option>
+        <option value="Jeollanam-do">전라남도</option>
+        <option value="Gyeongsangbuk-do">경상북도</option>
+        <option value="Gyeongsangnam-do">경상남도</option>
+        <option value="Jeju-do">제주도</option>
+      </select>
       {weather && (
         <div>
           <h2>{weather.name}</h2>
